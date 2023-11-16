68fc6a4201db93ad69f5365aa6610d960fa50113
wilhelm-lenz <wilhelm.v.lenz@gmail.com>
2023-11-16 15:43:13 +0100

    completed Project at the moment

--- assets/js/main.js
+++ assets/js/main.js
@@ -6,16 +6,15 @@ fetch("https://picsum.photos/v2/list")
     const sectionElement = document.querySelector(".gallary");
 
     for (let i = 0; i < data.length; i++) {
-      console.log(data);
-      console.log(sectionElement);
       const figureElement = document.createElement("figure");
       const imgElement = document.createElement("img");
       const figcaption = document.createElement("figcaption");
       const btnElement = document.createElement("button");
+      const btn = document.querySelectorAll(".cta-btn");
+
       imgElement.setAttribute("src", data[i].download_url);
       imgElement.setAttribute("alt", data[i].author);
       btnElement.classList.add("cta-btn");
-      const btn = document.querySelectorAll(".cta-btn");
 
       figcaption.textContent = data[i].author;
 
@@ -23,9 +22,7 @@ fetch("https://picsum.photos/v2/list")
       sectionElement.style.gridTemplateColumns =
         "repeat(auto-fill, minmax(400px, 1fr))";
       sectionElement.style.gap = "6rem";
-      imgElement.style.width = "100%";
-      imgElement.style.height = "100%";
-      figcaption.style.marginTop = "0.8rem";
+
       btnElement.textContent = "See more";
 
       figureElement.append(imgElement, figcaption, btnElement);
